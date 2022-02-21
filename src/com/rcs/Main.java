4e062f177e1c9b0daf0e51bebda4b482f56a38d0
RaitisPimanis <raitis.pimanis@gmail.com>
2022-02-21 13:34:10 +0200

    Initial commit

---
+++ src/com/rcs/Main.java
@@ -0,0 +1,25 @@
+package com.rcs;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double priceKwh = 0.20;
+//        int days = 21;
+        int lastMonth = 5228;
+
+        System.out.println("Ievadi skaitītāja rādījumu: ");
+        int insert_data = scan.nextInt();
+        System.out.println("Ievadi datumu: ");
+        int days = scan.nextInt();
+
+        int kwh = insert_data - lastMonth; // šomēness patērēts
+        double sum = kwh * priceKwh;
+        double daily = kwh / days;
+        System.out.println(kwh + " kwh patērētas šobrīd");
+        System.out.println( "Jāmaksā - " + sum + "eur" + " Vidējais elektrības patēriņš dienā - " + daily + "kwh");
+
+    }
+}
